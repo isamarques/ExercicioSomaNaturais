74561d69f5bfdc5c3273fb4de338df614f8b2f18
Isabela Marques <88113608+isamarques@users.noreply.github.com>
2022-08-18 22:16:51 -0300

    Add files via upload

---
+++ src/Controller/SomaNaturais.java
@@ -0,0 +1,22 @@
+//Feito em conjunto com Isabela Marques
+
+package Controller;
+
+public class SomaNaturais {
+
+	public SomaNaturais() {
+
+		super();
+	}
+
+	public int SomaN(int N) {
+		//condição de parada
+		if (N <= 0) {
+			return 0;
+
+		}
+		//função do termo anterior 
+		return N + SomaN(N - 1);
+	}
+
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,17 @@
+//Feito em conjunto com Isabela Marques
+
+package view;
+
+import Controller.SomaNaturais;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		SomaNaturais N = new SomaNaturais();
+		int SomaN = 10 ;
+		int resultado = N.SomaN(SomaN);
+	System.out.println(resultado);
+	}
+
+}
